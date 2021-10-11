80f5236f67af841a37045342d6c653fab54a19ed
Guillermo <knotfold@gmail.com>
2021-10-11 15:27:48 +0900

    Cube and capsule advances
    I made a bunch of changes here and there, but basically i added some new functions in the shape class, and used polymorphism to display different texts in the cube and capsule classes, also added encapsulation that i still don't really get properly

--- PTP/Assets/Scripts/Capsule.cs
+++ PTP/Assets/Scripts/Capsule.cs
@@ -2,35 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cube : Shape
+public class Capsule : Shape
 {
-    
+   
+   [SerializeField] string uses;
 
     public override IEnumerator DisplayText()
     {
-        
-            Debug.Log("hi i am  " + shapeType + " named: " + shapeName);
-       
-            return base.DisplayText();
-        
-
+        Debug.Log("I am a" + shapeType + " named: " + shapeName + "that you can use for: " + uses);
+        yield return new WaitForSeconds(2);
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        
-        // shapeName = cubeName;
-        // material = cubeMaterial;
         InitalizeShapeData();
         StartCoroutine(DisplayText());
-         InvokeRepeating("ChangeMaterial", 0.2f, 2.0f);
-         InvokeRepeating("Rotate", 1f, 2.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-      
-      
+        
     }
 }
--- PTP/Assets/Scripts/Shape.cs
+++ PTP/Assets/Scripts/Shape.cs
@@ -4,48 +4,131 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
-    public string shapeName{
-        get{return shapeName;}
-        set{
-            if(value == null || value.Trim() == " "){
+
+    [SerializeField] Material[] colors;
+    protected bool error = false;
+
+    public string shapeType
+    {
+        get { return m_shapeType; }
+        set { m_shapeType = value; }
+    }
+
+
+    private string m_shapeType;
+
+
+    public string shapeName
+    {
+        get { return m_ShapeName; }
+        set
+        {
+            if (value == null || value.Trim() == "")
+            {
                 Debug.LogError("The shape name cant be empty");
+                error = true;
             }
-            else{
+            else
+            {
                 m_ShapeName = value;
             }
         }
     }
+
+    [SerializeField]
     private string m_ShapeName;
-    public Material material{
-        get{return m_Material;}
-        set {m_Material = value;}
-    }
-    private Material m_Material;
-    public virtual IEnumerator DisplayText(){
-        bool cycle = true;
-        while(cycle){
-        yield return new WaitForSeconds(3);
-        Debug.Log("Im a " + m_ShapeName);
+    public Material material
+    {
+        get { return m_Material; }
+        [SerializeField]
+        set
+        {
+            if (value == null)
+            {
+                Debug.LogError("Material can not be null");
+                error = true;
+            }
+            else
+            {
+                m_Material = value;
+            }
         }
-       
     }
 
-    protected  void AssignMaterial(){
-         GetComponent<Renderer>().material = m_Material;
-    } 
+    [SerializeField]
+    private Material m_Material;
+    public virtual IEnumerator DisplayText()
+    {
+        if (error)
+        {
+            yield return new WaitForSeconds(0);
+        }
+        bool cycle = true;
+        while (cycle)
+        {
+            yield return new WaitForSeconds(3);
+            Debug.Log("Im a " + m_shapeType);
+        }
 
-    public void Rotate(){
+    }
+
+    protected virtual void InitalizeShapeData()
+    {
+        if (error)
+        {
+            return;
+        }
+        AssignShapeType();
+        AssignMaterial();
+    }
+
+    private void AssignShapeType()
+    {
+        shapeType = gameObject.tag;
+    }
+
+    private void AssignMaterial()
+    {
+        GetComponent<Renderer>().material = m_Material;
+    }
+
+
+
+    protected void Rotate()
+    {
+        if (error)
+        {
+            return;
+        }
         transform.Rotate(55.0f, 0.0f, 0.0f);
+
+
+    }
+
+    protected void ChangeMaterial()
+    {
+        if (error)
+        {
+            return;
+        }
+        if (colors.Length == 0)
+        {
+            Debug.LogError("The material color list is empty therefor the change material methot can not be used");
+        }
+        int randomIndex = Random.Range(0, colors.Length);
+        gameObject.GetComponent<MeshRenderer>().material = colors[randomIndex];
+        float randomScale = Random.Range(50f, 100f);
+        transform.localScale = new Vector3(randomScale, randomScale, randomScale);
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
